9d56f03fae06c1d1187677559d6a353205f5bb18
vjabhi00985 <princeabhi00985@gmail.com>
2025-04-12 21:37:12 +0530

    fixed: UI Components and HTML properly updated

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,11 @@
+// handle clear button click
 document.getElementById('clear-btn').addEventListener('click', () => {
   // Clear the input field
   document.getElementById('location').value = '';
 
-  // Clear the output text
-  document.getElementById('output').textContent = '';
+  // Clear the output
+  document.getElementById('output').innerHTML = '';
 
+  // Hide the loading animation
+  document.getElementById('loading').style.display = 'none';
 });
