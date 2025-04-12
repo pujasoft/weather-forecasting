1364916fb20048b9a670e0ce27c49ad10746ec60
vjabhi00985 <princeabhi00985@gmail.com>
2025-04-12 21:22:12 +0530

    Fix: Missing Html and UI updated

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+document.getElementById('clear-btn').addEventListener('click', () => {
+  // Clear the input field
+  document.getElementById('location').value = '';
+
+  // Clear the output text
+  document.getElementById('output').textContent = '';
+
+});
